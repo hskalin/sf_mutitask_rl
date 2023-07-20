corrected scaling

common/agent.py
@@ -55,9 +55,6 @@ class IsaacAgent:
         self.record = self.env_cfg["record"]
         self.save_model = self.env_cfg["save_model"]
 
-        self.w = torch.tile(w[0], (self.n_env, 1))
-        self.w_eval = torch.tile(w[1], (self.n_env, 1))  # [N, F]
-
         self.feature = feature
         self.observation_dim = self.env.num_obs
         self.feature_dim = self.feature.dim
@@ -65,6 +62,13 @@ class IsaacAgent:
         self.observation_shape = [self.observation_dim]
         self.feature_shape = [self.feature_dim]
         self.action_shape = [self.action_dim]
+
+        self.w = torch.tile(w[0], (self.n_env, 1))
+
+        if self.env_cfg["task"]["rand_weights"]:
+            self.w_eval = torch.rand((self.n_env, self.feature_dim), device=self.device)
+        else:
+            self.w_eval = torch.tile(w[1], (self.n_env, 1))  # [N, F]
 
         self.per = self.buffer_cfg["prioritize_replay"]
 
@@ -117,6 +121,53 @@ class IsaacAgent:
             if self.steps > self.total_timesteps:
                 break
 
+    def randomizeTrainWeights(self):
+        randType = self.env_cfg["task"]["task_w_randType"]
+
+        if randType == "uniform":
+            self.w = torch.rand((self.n_env, self.feature_dim), device=self.device)
+
+        elif randType == "ea":
+            task_w_ea = torch.tensor(
+                self.env_cfg["task"]["task_w_ea"], device=self.device
+            )
+            parse = int(self.n_env / len(task_w_ea))
+            for i in range(len(task_w_ea)):
+                self.w[i * parse : (i + 1) * parse, :] = task_w_ea[i]
+
+        elif randType == "uni_ea":
+            self.w = torch.rand((self.n_env, self.feature_dim), device=self.device)
+            task_w_ea = torch.tensor(
+                self.env_cfg["task"]["task_w_ea"], device=self.device
+            )
+            parse = int(self.n_env / len(task_w_ea) * 0.5)
+            parse0 = int(self.n_env / 2)
+
+            for i in range(len(task_w_ea)):
+                self.w[parse0 + i * parse : parse0 + (i + 1) * parse, :] = task_w_ea[i]
+
+        elif randType == "randbasis":
+            import itertools
+
+            l = list(itertools.product([0, 1], repeat=self.feature_dim))
+            es = torch.tensor(l, device=self.device)
+            parse = int(self.n_env / len(l))
+
+            for i in range(len(l)):
+                self.w[i * parse : (i + 1) * parse, :] = es[i]
+
+        elif randType == "uni_randbasis":
+            import itertools
+
+            self.w = torch.rand((self.n_env, self.feature_dim), device=self.device)
+
+            l = list(itertools.product([0, 1], repeat=self.feature_dim))
+            es = torch.tensor(l, device=self.device)
+            parse = int(self.n_env / len(l) * 0.5)
+            parse0 = int(self.n_env / 2)
+            for i in range(len(l)):
+                self.w[parse0 + i * parse : parse0 + (i + 1) * parse, :] = es[i]
+
     def train_episode(self, gui_app=None, gui_rew=None):
         self.episodes += 1
         episode_r = episode_steps = 0
@@ -124,6 +175,10 @@ class IsaacAgent:
 
         print("episode = ", self.episodes)
         print(self.w[0])
+
+        if (self.env_cfg["mode"] == "train") and (self.env_cfg["task"]["rand_weights"]):
+            # self.w = torch.rand((self.n_env, self.feature_dim), device=self.device)
+            self.randomizeTrainWeights()
 
         s = self.reset_env()
         for _ in range(self.episode_max_step):
@@ -168,9 +223,6 @@ class IsaacAgent:
 
             if episode_steps >= self.episode_max_step:
                 break
-
-        if (self.env_cfg["mode"] == "train") and (self.env_cfg["task"]["rand_weights"]):
-            self.w = torch.rand((self.n_env, self.feature_dim), device=self.device)
 
         # if self.episodes % self.log_interval == 0:
         wandb.log({"reward/train": self.game_rewards.get_mean()})
@@ -223,6 +275,7 @@ class IsaacAgent:
                 self.env.reset()
 
                 r = self.calc_reward(s_next, self.w_eval)
+                print(self.w_eval[0])
 
                 s = s_next
                 episode_r += r

common/compositions.py
@@ -14,6 +14,9 @@ class Compositions:
         self.n_heads = n_heads
         self.feature_dim = n_heads
         self.action_dim = action_dim
+        self.sf_norm_coeff = torch.ones(
+            (self.feature_dim), device=torch.device("cuda:0"), dtype=torch.float32
+        )
 
         self.impact_x_idx = []  # record primitive impact on x-axis
         self.policy_idx = []  # record primitive summon frequency
@@ -164,8 +167,11 @@ class Compositions:
     def gpe(self, s, acts, w):
         # [N, Ha, Hsf, F] <-- [N, S], [N, Ha, A]
         curr_sf = self.calc_curr_sf(s, acts)
+        gating = self.scale_gating(w)  # [N, Ha], H=F
+
         # [N,Ha,Hsf]<--[N,Ha,Hsf,F],[N,F]
-        qs = torch.einsum("ijkl,il->ijk", curr_sf.float(), w.float())
+        # qs = torch.einsum("ijkl,il->ijk", curr_sf.float(), w.float())
+        qs = torch.einsum("ijkl,il->ijk", curr_sf.float(), gating)
         return qs  # [N,Ha,Hsf]
 
     def gpi(self, acts, value, rule="q"):
@@ -215,7 +221,8 @@ class Compositions:
         return curr_sf  # [N, Ha, Hsf, F]
 
     def scale_gating(self, gating):
-        return torch.softmax(gating / gating.shape[1], 1)
+        # return torch.softmax(gating / gating.shape[1], 1)
+        return gating / (gating.norm(1, 1, keepdim=True) * self.sf_norm_coeff)
 
     def calc_advantage(self, value):  # [N,Ha,Hsf,F]
         adv = value.mean(1, keepdim=True) - value.mean((1, 2), keepdim=True)

common/feature.py
@@ -1,42 +1,62 @@
 import torch
 from scipy.spatial.transform import Rotation as R
+import numpy as np
 
 
 class pm_feature:
     def __init__(
         self,
-        combination=[True, False, False, False, False, False, False],
-        dim=3,
+        env_cfg,
+        combination=[True, True, True, True],
     ) -> None:
-        self.envdim = dim
+        self.env_cfg = env_cfg
+        self.envdim = env_cfg["dim"]
 
-        self._pos_err = combination[0]
-        self._pos_norm = combination[1]
-        self._vel_err = combination[2]
-        self._vel_norm = combination[3]
+        self._pos_norm = combination[0]
+        self._vel_err = combination[1]
+        self._vel_norm = combination[2]
+        self._prox = combination[3]
 
         self.dim = (
-            self.envdim * combination[0]  # pos
-            + combination[1]  # pos_norm
-            + self.envdim * combination[2]  # vel
-            + combination[3]  # vel_norm
+            combination[0]  # pos
+            + self.envdim * combination[1]  # vel_err
+            + combination[2]  # vel_norm
+            + combination[3]  # prox
         )
 
     def extract(self, s):
         features = []
 
-        pos = s[:, 0 : self.envdim]
-        if self._pos_err:
-            features.append(-torch.abs(pos))
-        if self._pos_norm:
-            features.append(-torch.linalg.norm(pos, axis=1, keepdims=True))
+        Kp = self.env_cfg["goal_lim"]
+        Kv = self.env_cfg["vel_lim"]
+        prox_thresh = self.env_cfg["task"]["proximity_threshold"]
 
-        # vel = s[:, 12:15]
+        pos = s[:, 0 : self.envdim]
+        posSquaredNorm = torch.linalg.norm(pos, axis=1, keepdims=True) ** 2
+        posNormFeature = (
+            torch.exp(-7.2 * posSquaredNorm / Kp**2)
+            + torch.exp(-360 * posSquaredNorm / Kp**2)
+        ) / 2
+        if self._pos_norm:
+            features.append(posNormFeature)
+
         vel = s[:, self.envdim : 2 * self.envdim]
         if self._vel_err:
-            features.append(-torch.abs(vel))
+            vel_feature = posNormFeature * torch.exp(-7.2 * vel**2 / Kv**2)
+            features.append(vel_feature)
+
         if self._vel_norm:
-            features.append(-torch.linalg.norm(vel, axis=1, keepdims=True))
+            velSquaredNorm = s[:, 4:5] ** 2
+            velNormFeature = posNormFeature * torch.exp(-7.2 * velSquaredNorm / Kv**2)
+            features.append(velNormFeature)
+
+        if self._prox:
+            A = 1 / np.exp(-12.8 * prox_thresh**2 / Kp**2)
+            proximity_rew_gauss = A * torch.exp(-12.8 * posSquaredNorm / Kp**2)
+            proximity_rew = torch.where(
+                posSquaredNorm > prox_thresh**2, proximity_rew_gauss, 1
+            )
+            features.append(proximity_rew)
 
         return torch.cat(features, 1)
 
@@ -48,14 +68,6 @@ class pointer_feature:
         combination=[True, True, True, True],
     ) -> None:
         self.envdim = env_cfg["dim"]
-
-        # self._pos_err = combination[0]
-        # self._pos_norm = combination[1]
-        # self._vel_err = combination[2]
-        # self._vel_norm = combination[3]
-        # self._ang_err = combination[4]
-        # self._angvel_err = combination[5]
-        # self._success = combination[6]
 
         self._pos_norm = combination[0]
         self._vel_norm = combination[1]

env/pointMass2D.py
@@ -13,15 +13,13 @@ import os
 class PointMass2D(VecEnv):
     def __init__(self, cfg):
         # task-specific parameters
-        self.num_obs = 4
+        self.num_obs = 4 + 1
         self.num_act = 2
         self.reset_dist = 10.0  # when to reset
         self.max_push_effort = 5.0  # the range of force applied to the pointer
 
         self.ball_height = 2
-        self.goal_lim = 10
-        self.vel_lim = 5
-        self.goal_vel_lim = 2
+        self.goal_vel_lim = 5
 
         super().__init__(cfg=cfg)
 
@@ -120,16 +118,22 @@ class PointMass2D(VecEnv):
         self.gym.refresh_rigid_body_state_tensor(self.sim)
 
         # relative xyz pos
-        pos = self.rb_pos[env_ids, 0] - self.goal_pos[env_ids]
+        relativePos = self.rb_pos[env_ids, 0] - self.goal_pos[env_ids]
 
-        self.obs_buf[env_ids, 0] = pos[env_ids, 0]
-        self.obs_buf[env_ids, 1] = pos[env_ids, 1]
+        self.obs_buf[env_ids, 0] = relativePos[env_ids, 0]
+        self.obs_buf[env_ids, 1] = relativePos[env_ids, 1]
 
         # relative xyz vel
-        vel = self.rb_lvels[env_ids, 0] - self.goal_lvel[env_ids]
+        relativeVel = self.rb_lvels[env_ids, 0] - self.goal_lvel[env_ids]
 
-        self.obs_buf[env_ids, 2] = vel[env_ids, 0]
-        self.obs_buf[env_ids, 3] = vel[env_ids, 1]
+        self.obs_buf[env_ids, 2] = relativeVel[env_ids, 0]
+        self.obs_buf[env_ids, 3] = relativeVel[env_ids, 1]
+
+        vel_norm = torch.linalg.norm(
+            self.rb_lvels[env_ids, 0], axis=1, keepdims=False
+        ) - torch.linalg.norm(self.goal_lvel[env_ids], axis=1, keepdims=False)
+
+        self.obs_buf[env_ids, 4] = vel_norm
 
     def get_reward(self):
         # retrieve environment observations from buffer
@@ -315,30 +319,30 @@ def compute_point_reward(
 ):
     # type: (Tensor, Tensor, float, Tensor, Tensor, Tensor, Tensor, float) -> Tuple[Tensor, Tensor, Tensor, Tensor]
 
-    # square distance from goal
+    # # square distance from goal
     sqr_dist = (x_pos) ** 2 + (y_pos) ** 2
 
-    # Proximity reward
-    A1 = 1
-    B1 = (2 + torch.log(A1)) / (8**2)
+    # # Proximity reward
+    # A1 = 1
+    # B1 = (2 + torch.log(A1)) / (8**2)
 
-    proximity_rew = (torch.exp(-B1 * sqr_dist) + torch.exp(-3 * sqr_dist)) / 2
+    # proximity_rew = (torch.exp(-B1 * sqr_dist) + torch.exp(-3 * sqr_dist)) / 2
 
-    # Total
-    reward = A1 * proximity_rew
+    # # Total
+    reward = -sqr_dist
 
-    # print(proximity_rew[0], angle_rew[0], rot_rew[0])
-    # print(reward[0])
+    # # print(proximity_rew[0], angle_rew[0], rot_rew[0])
+    # # print(reward[0])
 
-    # adjust reward for reset agents
+    # # adjust reward for reset agents
     # reward = torch.where(z_abs < 0.75, torch.ones_like(reward) * -200.0, reward)
-    # reward = torch.where(torch.abs(x_pos) > reset_dist, torch.ones_like(reward) * -200.0, reward)
-    # reward = torch.where(torch.abs(y_pos) > reset_dist, torch.ones_like(reward) * -200.0, reward)
-    # reward = torch.where(torch.abs(wz) > 45, torch.ones_like(reward) * -200.0, reward)
-    # reward = torch.where(x_action < 0, reward - 0.1, reward)
-    # reward = torch.where((torch.abs(x_pos) < 0.1) & (torch.abs(y_pos) < 0.1), reward + 1, reward)
+    # # reward = torch.where(torch.abs(x_pos) > reset_dist, torch.ones_like(reward) * -200.0, reward)
+    # # reward = torch.where(torch.abs(y_pos) > reset_dist, torch.ones_like(reward) * -200.0, reward)
+    # # reward = torch.where(torch.abs(wz) > 45, torch.ones_like(reward) * -200.0, reward)
+    # # reward = torch.where(x_action < 0, reward - 0.1, reward)
+    # # reward = torch.where((torch.abs(x_pos) < 0.1) & (torch.abs(y_pos) < 0.1), reward + 1, reward)
 
-    return_buf += reward
+    # return_buf += reward
 
     reset = torch.where(
         torch.abs(sqr_dist) > 100, torch.ones_like(reset_buf), reset_buf